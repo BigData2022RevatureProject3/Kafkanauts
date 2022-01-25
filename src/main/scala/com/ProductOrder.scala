68945d921038f03278f28eb913a8149f1d3990d2
David-Carlson <David.Carlson2012@gmail.com>
2022-01-25 10:21:34 -0700

    added data/changed case class

--- src/main/scala/com/ProductOrder.scala
+++ src/main/scala/com/ProductOrder.scala
@@ -1,20 +1,20 @@
 package com
 
 case class ProductOrder(
-                       order_id: Long,
-                       customer_id: Long,
-                       customer_name: String,
-                       product_id: String,
-                       product_name: String,
-                       product_category: String,// String?
-                       payment_type: String, // huh?
-                       qty: Long,
-                       price: Double,
-                       datetime: String,
-                       country: String,
-                       city: String,
-                       ecommerce_website_name: String,
-                       payment_txn_id: Long,
-                       payment_txn_success: String,
-                       failure_reason: String
-                       )
+   order_id: Long,
+   customer_id: Long,
+   customer_name: String,
+   product_id: String,
+   product_name: String,
+   product_category: String,// String?
+   payment_type: String, // huh?
+   qty: Long,
+   price: Double,
+   datetime: String,
+   country: String,
+   city: String,
+   ecommerce_website_name: String,
+   payment_txn_id: Long,
+   payment_txn_success: String,
+   failure_Reason: String
+   )
