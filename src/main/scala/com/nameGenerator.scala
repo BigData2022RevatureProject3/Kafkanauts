b3eb1ccc90315fc6a4dd4c32bc863dd1cd8d0191
baodoan95 <95301717+baodoan95@users.noreply.github.com>
2022-01-26 13:45:13 -0600

    Update nameGenerator.scala
    Add Chinese and European Name Generator methods into nameGenerator Class

--- src/main/scala/com/nameGenerator.scala
+++ src/main/scala/com/nameGenerator.scala
@@ -1,21 +1,19 @@
 package com
 import scala.collection.mutable.ListBuffer
 import scala.io.Source
-object nameGenerator extends App{
-  val firstFile = "data/first_name_american_common_female.txt"
-  val lastFile = "data/last_name_american_common.txt"
-  val firstNameList = new ListBuffer[String]
-  val lastNameList = new ListBuffer[String]
-  for (line <- Source.fromFile(firstFile).getLines) {
-    val name = line.toLowerCase().split(",")
-    firstNameList += name(1).capitalize
-  }
-  for (line <- Source.fromFile(lastFile).getLines) {
-    val name = line.toLowerCase().split(",")
-    lastNameList += name(1).capitalize
-  }
+class nameGenerator{
 
-  def genName():String ={
+  def genAmericanName():String ={
+    val firstFile = "data/first_name_american_common_female.txt"
+    val lastFile = "data/last_name_american_common.txt"
+    val firstNameList = new ListBuffer[String]
+    val lastNameList = new ListBuffer[String]
+    for (line <- Source.fromFile(firstFile).getLines) {
+      val name = line.toLowerCase().split(",")
+      firstNameList += name(1).capitalize}
+    for (line <- Source.fromFile(lastFile).getLines) {
+      val name = line.toLowerCase().split(",")
+      lastNameList += name(1).capitalize}
     val ran = scala.util.Random
     val fName = firstNameList(ran.nextInt(firstNameList.length))
     val lName = lastNameList(ran.nextInt(lastNameList.length))
@@ -23,5 +21,34 @@ object nameGenerator extends App{
     name
   }
 
-  print(genName())
+  def genChineseName():String={
+    val firstFile = "data/first_name_chinese(given names).txt"
+    val lastFile = "data/last_name_chinese(family name).txt"
+    val firstNameList = new ListBuffer[String]
+    val lastNameList = new ListBuffer[String]
+    for (line <- Source.fromFile(firstFile).getLines) {firstNameList += line}
+    for (line <- Source.fromFile(lastFile).getLines) {lastNameList += line}
+    val ran = scala.util.Random
+    val fName = firstNameList(ran.nextInt(firstNameList.length))
+    val lName = lastNameList(ran.nextInt(lastNameList.length))
+    val name = fName + lName
+    name
+  }
+
+  def genEuropeanName():String={
+    val firstFile = "data/first_name_european.txt"
+    val lastFile = "data/last_name_american_common.txt"
+    val firstNameList = new ListBuffer[String]
+    val lastNameList = new ListBuffer[String]
+    for (line <- Source.fromFile(firstFile).getLines) {firstNameList += line}
+    for (line <- Source.fromFile(lastFile).getLines) {
+      val name = line.toLowerCase().split(",")
+      lastNameList += name(1).capitalize}
+    val ran = scala.util.Random
+    val fName = firstNameList(ran.nextInt(firstNameList.length))
+    val lName = lastNameList(ran.nextInt(lastNameList.length))
+    val name = fName +" " + lName
+    name
+  }
+
 }
