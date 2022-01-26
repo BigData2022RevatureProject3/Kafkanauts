c10dbe2ff7562caf36c24ccb603948b111ba3e60
Benjamin Ruiz <benjaminruiz@g.ucla.edu>
2022-01-26 14:45:09 -0800

    Created orderRegex class with isValidOrder function that takes in a String order and applies a regex on it to determine if the order is invalid and if so returns false, else true

---
+++ src/main/scala/com/Tools/orderRegex.scala
@@ -0,0 +1,12 @@
+package com.Tools
+
+class orderRegex {
+  protected def isValidOrder(order:String):Boolean ={
+    val pattern = "^\\d+[,]\\d+[,]\\w+\\s\\w+,\\d+,\\w+,\\w+,\\w+,\\d+,\\d+.\\d+,\\d+-\\d+-\\d+\\s\\d+:\\d+,\\w+,\\w+\\s\\w+,\\w+.\\w+,\\d+,[YN],\\w+$".r
+    val result = (pattern findAllIn order)
+    if (result.isEmpty) {
+      return false
+    }
+    return true
+  }
+}
