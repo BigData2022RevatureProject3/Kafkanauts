b641a82b4a062635f0e5ba04f5a53ba27aa4e582
baodoan95 <baodoan95@gmail.com>
2022-01-27 11:16:49 -0600

    Changed name generator to full customer information generator

--- src/main/scala/com/Tools/customerInfoGenerator.scala
+++ src/main/scala/com/Tools/customerInfoGenerator.scala
@@ -1,16 +1,21 @@
 package com.Tools
 import scala.collection.mutable.ListBuffer
 import scala.io.Source
-class idNameGenerator{
-  
+class customerInfoGenerator{
   private final val fUsFile = "data/first_name_american_common_female.txt"
   private final val lUsFile = "data/last_name_american_common.txt"
   private final val fCnFile = "data/first_name_chinese(given names).txt"
   private final val lCnFile = "data/last_name_chinese(family name).txt"
   private final val fEuFile = "data/first_name_european.txt"
+  private final val usCityFile = "data/american_cities.txt"
+  private final val cnCityFile = "data/chinese_cities.txt"
+  private final val spCityFile = "data/spain_cities.txt"
+  private final val countries = new ListBuffer[String]
   private final val ran = scala.util.Random
+  for (line <- Source.fromFile("data/countries.txt").getLines) {
+    countries += line }
 
-  protected def genAmericanName():String ={
+  protected def genUSCustomer():String ={
     val firstNameList = new ListBuffer[String]
     val lastNameList = new ListBuffer[String]
     for (line <- Source.fromFile(fUsFile).getLines) {
@@ -22,10 +27,14 @@ class idNameGenerator{
     val fName = firstNameList(ran.nextInt(firstNameList.length))
     val lName = lastNameList(ran.nextInt(lastNameList.length))
     val name = fName + " " + lName
-    name.hashCode()+","+name
+    val cities = new ListBuffer[String]
+    val country = countries(0)
+    for (line <- Source.fromFile(usCityFile).getLines) {
+      cities += line }
+    Math.abs(name.hashCode())+","+name+","+cities(ran.nextInt(cities.length+1)) +","+ country
   }
 
-  protected def genChineseName():String={
+  protected def genCNCustomer():String={
     val firstNameList = new ListBuffer[String]
     val lastNameList = new ListBuffer[String]
     for (line <- Source.fromFile(fCnFile).getLines) {firstNameList += line}
@@ -33,10 +42,14 @@ class idNameGenerator{
     val fName = firstNameList(ran.nextInt(firstNameList.length))
     val lName = lastNameList(ran.nextInt(lastNameList.length))
     val name = fName + lName
-    name.hashCode()+","+name
+    val cities = new ListBuffer[String]
+    val country = countries(1)
+    for (line <- Source.fromFile(cnCityFile).getLines) {
+      cities += line }
+    Math.abs(name.hashCode())+","+name+","+cities(ran.nextInt(cities.length+1)) +","+ country
   }
 
-  protected def genEuropeanName():String={
+  protected def genEUCustomer():String={
     val firstNameList = new ListBuffer[String]
     val lastNameList = new ListBuffer[String]
     for (line <- Source.fromFile(fEuFile).getLines) {firstNameList += line}
@@ -46,6 +59,10 @@ class idNameGenerator{
     val fName = firstNameList(ran.nextInt(firstNameList.length))
     val lName = lastNameList(ran.nextInt(lastNameList.length))
     val name = fName +" " + lName
-    name.hashCode()+","+name
+    val cities = new ListBuffer[String]
+    val country = countries(2)
+    for (line <- Source.fromFile(spCityFile).getLines) {
+      cities += line }
+    Math.abs(name.hashCode())+","+name+","+cities(ran.nextInt(cities.length)+1) +","+ country
   }
 }
