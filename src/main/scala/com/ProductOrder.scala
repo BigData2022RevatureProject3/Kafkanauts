af8adb82c9249974b02bacad3a28d336c6097b91
David-Carlson <David.Carlson2012@gmail.com>
2022-01-27 09:50:07 -0700

    Made Product order use var, added companion object

--- src/main/scala/com/ProductOrder.scala
+++ src/main/scala/com/ProductOrder.scala
@@ -1,20 +1,26 @@
 package com
 
 case class ProductOrder(
-   order_id: Long,
-   customer_id: Long,
-   customer_name: String,
-   product_id: String,
-   product_name: String,
-   product_category: String,// String?
-   payment_type: String, // huh?
-   qty: Long,
-   price: Double,
-   datetime: String,
-   country: String,
-   city: String,
-   ecommerce_website_name: String,
-   payment_txn_id: Long,
-   payment_txn_success: String,
-   failure_reason: String
+   var order_id: Long,
+   var customer_id: Long,
+   var customer_name: String,
+   var product_id: Long,
+   var product_name: String,
+   var product_category: String,// String?
+   var payment_type: String, // huh?
+   var qty: Long,
+   var price: Double,
+   var datetime: String,
+   var country: String,
+   var city: String,
+   var ecommerce_website_name: String,
+   var payment_txn_id: Long,
+   var payment_txn_success: String,
+   var failure_reason: String
    )
+
+case object  ProductOrder {
+   def getSampleOrder(): ProductOrder = {
+      ProductOrder(1, 2, "Bob Burr", 3, "pname", "pcategory", "Card", 20, 9.99, "2004-05-23T14:25:10", "U.S", "Flagstaff", "google.com", 234, "Y", "")
+   }
+}
