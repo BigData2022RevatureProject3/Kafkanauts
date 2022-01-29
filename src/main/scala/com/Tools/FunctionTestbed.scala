c7f5df510eb8b794dec1c6a0ceb850e6ebe87d52
David-Carlson <David.Carlson2012@gmail.com>
2022-01-29 13:24:31 -0700

    Adding sparkhelper/function creation methods

--- src/main/scala/com/Tools/FunctionTestbed.scala
+++ src/main/scala/com/Tools/FunctionTestbed.scala
@@ -1,12 +1,22 @@
 package com.Tools
 
+import org.apache.spark.sql.DataFrame
+
 object FunctionTestbed {
   def main(args: Array[String]): Unit = {
-    val
+    val normal = MathHelper.getNormalPDF(0.5, 0.2)
+    functionToDataFrame(normal).show()
+    
+  }
 
+  def functionToDataFrame(func: (Double) => Double, samples: Int = 10, initialX: Double = 0.0): DataFrame = {
+    val spark = SparkHelper.spark
+    import spark.implicits._
 
-
-
+    var step = (1.0 - initialX) / samples
+    val x = (initialX to 1.0 by step)
+    val y = x.map(func)
+    spark.sparkContext.parallelize(x.zip(y)).toDF()
   }
 
 }
