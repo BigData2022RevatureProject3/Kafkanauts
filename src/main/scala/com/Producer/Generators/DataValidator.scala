da8f5d9f83fe6e84302c6b619f096dade346d3ff
Teddy Rosemond <38147937+trosemond13@users.noreply.github.com>
2022-01-31 13:54:24 -0500

    updated DataValidator

--- src/main/scala/com/Producer/Generators/DataValidator.scala
+++ src/main/scala/com/Producer/Generators/DataValidator.scala
@@ -3,7 +3,11 @@ package com.Producer.Generators
 import os.RelPath
 
 object DataValidator {
-
+  /**
+   * Checks if the file is validated or not. Returns true if the file is pipe-delimited and false if not.
+   * @param filePath The relative path of the file in the
+   * @return Boolean: True or false depending on if the file is tab-delimited or not.
+   */
   private def isValid(filePath: os.pwd.ThisType): Boolean = {
     //If the file exists, check if it is pipe['|'] delimited
     if (os.isFile(filePath)) {
@@ -16,15 +20,33 @@ object DataValidator {
     false
   }
 
-  def validatedData(relativeFilePath: String): Unit = {
-    val filePath = os.pwd / RelPath(relativeFilePath)
+  /**
+   * This method is used to turn comma-delimited files into pipe-delimited files and returns the filepath of the file.
+   * If the original filepath is not in the a member of the clean_data folder, it will make a copy of the original file
+   * and place it in the clean_data directory before replacing all the commas for pipes for version control purposes.
+   * If the file is already valid, it will just return the filepath.
+   * @param relativeFilePath
+   * @return os.pwd.ThisType: The file path of the file as a ThisType.
+   */
+  def validatedData(relativeFilePath: String): os.pwd.ThisType = {
+    var filePath = os.pwd / RelPath(relativeFilePath)
+    if (relativeFilePath.split("/").head != "clean_data") {
+      //println("Validate Data Log, Copy file from pwd/data to pwd/clean_data and validating it.")
+      val cleanFilePath = os.pwd / "clean_data" / relativeFilePath.split("/").tail
+      //println("cleaned file path", cleanFilePath)
+      os.copy(filePath, cleanFilePath)
+      filePath = cleanFilePath
+    }
+
     if(!isValid(filePath)) {
       val validatedText = os
         .read
         .lines(filePath)
-        .map(_.replace(",", "|")).mkString("\n")
+        .map(_.replace(",", "|"))
+        .mkString("\n")
       os.write.over(filePath, validatedText)
     }
+    filePath
   }
 
 
@@ -40,6 +62,6 @@ object DataValidator {
   }
 
   def main(args: Array[String]): Unit = {
-    validatedData("clean_data/ecommerce_cleaned_teddy.txt")
+    validatedData("data/hello.txt")
   }
 }
