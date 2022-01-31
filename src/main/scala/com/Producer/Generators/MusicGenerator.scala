8aa6967e0b615b54d0ec4fbd49ca74dc632c41d6
baodoan95 <95301717+baodoan95@users.noreply.github.com>
2022-01-31 16:03:32 -0600

    Update MusicGenerator.scala

--- src/main/scala/com/Producer/Generators/MusicGenerator.scala
+++ src/main/scala/com/Producer/Generators/MusicGenerator.scala
@@ -1,10 +1,6 @@
 package com.Producer.Generators
 
 import com.ProductOrder
-
-import scala.collection.mutable.ListBuffer
-import scala.io.Source
-
 object MusicGenerator {
   private final val ran = scala.util.Random
   private final val musicFile = os.read.lines(os.pwd/"clean_data"/"track_info.txt").drop(1)
