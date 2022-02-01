7ac14cd397ba48babc4cbfd718e44c2554d76c14
Benjamin Ruiz <benjaminruiz@g.ucla.edu>
2022-02-01 08:53:03 -0800

    Added support for products with multiple space separated words names and countries with either one word or multi word names

--- src/main/scala/com/ProductOrder.scala
+++ src/main/scala/com/ProductOrder.scala
@@ -57,7 +57,7 @@ object  ProductOrder {
    }
 
    def isValidOrder(order:String):Boolean ={
-      val pattern = "^\\d+[|]\\d+[|]\\w+\\s\\w+[|]\\d+[|]\\w+[|]\\w+[|]\\w+[|]\\d+[|]\\d+.\\d+[|]\\d+-\\d+-\\d+\\s\\d+:\\d+[|]\\w+[|]\\w+\\s\\w+[|]\\w+.\\w+[|]\\d+[|][YN][|]\\w+$".r
+      val pattern = "^\\d+[|]\\d+[|]\\w+\\s\\w+[|]\\d+[|]\\w+(\\s*\\w*)*[|]\\w+[|]\\w+[|]\\d+[|]\\d+.\\d+[|]\\d+-\\d+-\\d+\\s\\d+:\\d+[|]\\w+\\s*\\w*[|]\\w+\\s*\\w*[|]\\w+.\\w+[|]\\d+[|][YN][|]\\w+$".r
       val result = (pattern findAllIn order)
       result.nonEmpty
    }
