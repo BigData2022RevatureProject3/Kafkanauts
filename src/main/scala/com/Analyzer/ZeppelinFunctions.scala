6da3205cc16f5f00a50e803bdfeefab0a5f48141
David-Carlson <David.Carlson2012@gmail.com>
2022-02-03 15:53:49 -0700

    Better query format

--- src/main/scala/com/Analyzer/ZeppelinFunctions.scala
+++ src/main/scala/com/Analyzer/ZeppelinFunctions.scala
@@ -3,10 +3,12 @@ package com.Analyzer
 object ZeppelinFunctions extends App{
 
   //  A place to paste scala functions (like for loading our data), as well as sql strings
-  //Load data into dataframe in zeppelin
-  /*val text = sc.textFile("hdfs://localhost:9000/user/steve_rev/valid_products.csv")
-  val data = text.toDF().select("*")
-  val df2 = data.select(
+  // Load data into dataframe in zeppelin
+  """"
+
+    "val text = sc.textFile("hdfs://localhost:9000/user/steve_rev/valid_products.csv")
+    val data = text.toDF().select("*")
+    val df2 = data.select(
     split(col("value"), "\\|").getItem(0).as("order_id").cast("long"),
     split(col("value"), "\\|").getItem(1).as("customer_id").cast("long"),
     split(col("value"), "\\|").getItem(2).as("customer_name").cast("string"),
@@ -24,19 +26,29 @@ object ZeppelinFunctions extends App{
     split(col("value"), "\\|").getItem(14).as("payment_txn_success").cast("string"),
     split(col("value"), "\\|").getItem(15).as("failure_reason").cast("string")
   ).drop("value")
-  df2.createOrReplaceTempView("products")*/
+  df2.createOrReplaceTempView("products")
+  """
 
 
   //Bao SQL Code Portion
-  /*SELECT SUBSTRING(datetime,1,10) as date,country,COUNT(product_id) AS total_orders FROM products GROUP BY date, country HAVING country NOT IN("null") AND date NOT IN ("null") ORDER BY date,total_orders DESC;
-SELECT SUBSTRING(datetime,1,10) as date,product_category,COUNT(product_id) AS total_orders FROM products GROUP BY date, product_category HAVING product_category IN("Electronics","Entertainment","Computers","Food","Home")AND date NOT IN ("null") ORDER BY date,total_orders DESC;
-*/
+  """
+
+  SELECT SUBSTRING(datetime,1,10) as date,country,COUNT(product_id) AS total_orders FROM products GROUP BY date, country HAVING country NOT IN("null") AND date NOT IN ("null") ORDER BY date,total_orders DESC;
+  SELECT SUBSTRING(datetime,1,10) as date,product_category,COUNT(product_id) AS total_orders FROM products GROUP BY date, product_category HAVING product_category IN("Electronics","Entertainment","Computers","Food","Home")AND date NOT IN ("null") ORDER BY date,total_orders DESC;
+
+  """
+
+
   //Ben SQL Code Portion
   //Brady SQL Code Portion
   //David SQL Code Portion
   //Teddy SQL Code Portion
   //Tiffany SQL Code Portion
+
   //Steven SQL Code Portion
-  /*SELECT distinct product_category, COUNT(product_category) as Number_Of_Products FROM products Where product_category In ("Electronics", "Computers", "Food", "Entertainment", "Home") GROUP By product_category;
-   */
+  """
+
+  SELECT distinct product_category, COUNT(product_category) as Number_Of_Products FROM products Where product_category In ("Electronics", "Computers", "Food", "Entertainment", "Home") GROUP By product_category;
+
+  """
 }
